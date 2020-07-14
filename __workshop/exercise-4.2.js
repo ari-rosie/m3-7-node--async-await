eabd07a1f0394ddb7270e46db73ea9bd25880747
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-07-13 22:18:39 -0400

    Exercice-4.2

--- __workshop/exercise-4.2.js
+++ __workshop/exercise-4.2.js
@@ -1,5 +1,17 @@
 const request = require('request-promise');
 
 const getTronaldDumpQuote = async () => {
-  // write write write
+  const reqObj = {
+    uri: 'https://api.tronalddump.io/random/quote',
+    headers: {"Accept": "application/json"},
+    json: true 
+  }
+  try {
+    const reqJoke = await request(reqObj);
+    return reqJoke.value;
+  } catch (err) {
+    console.log(err);
+  }
 };
+
+getTronaldDumpQuote().then(res => console.log(res));
