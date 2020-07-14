42889100dc6364fd1e3b57855fce3e59daf40877
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-07-13 21:55:57 -0400

    Exercice-4.1

--- __workshop/exercise-4.1.js
+++ __workshop/exercise-4.1.js
@@ -1,6 +1,19 @@
 const request = require('request-promise');
+const json = require('body-parser/lib/types/json');
 
-// getDadJoke
 
-// 4.1
-// getJoke().then((data) => console.log(data));
+const getDadJoke = async () => {
+    const reqObj = {
+        headers:{"Accept" : "application/json"},
+        uri: 'https://icanhazdadjoke.com/',
+        json: true
+    }
+    try {
+        const reqJoke = await request(reqObj);
+        return reqJoke.joke;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+getDadJoke().then(res => console.log((res)));
