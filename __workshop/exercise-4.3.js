5240ca879b2ebd5235e23162f9b9056e60e01f17
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-07-13 22:24:55 -0400

    Exercice-4.3

--- __workshop/exercise-4.3.js
+++ __workshop/exercise-4.3.js
@@ -1,5 +1,17 @@
 const request = require('request-promise');
 
 const getGeekJoke = async () => {
-  // ha hahahh
+  const reqObj = {
+    uri: 'https://geek-jokes.sameerkumar.website/api?format=json',
+    json: true
+  }
+
+  try {
+    const reqJoke = await request(reqObj);
+    return reqJoke.joke;
+  } catch (err) {
+    console.log(err)
+  }
 };
+
+getGeekJoke().then(res => console.log(res));
